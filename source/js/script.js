ee2c42c0510e29c63fbdb006032cbaa8d9a4dd4f
juanacardo <juanacardo@gmail.com>
2021-10-15 17:10:12 +0500

    Шапка, навигация и первый экран

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var navigation = document.querySelector('.main-navigation');
+var navToggle = navigation.querySelector('.main-navigation__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+navigation.classList.remove('main-navigation--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navToggle.addEventListener('click', function () {
+  if (navigation.classList.contains('main-navigation--closed')) {
+    navigation.classList.remove('main-navigation--closed');
+    navigation.classList.add('main-navigation--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navigation.classList.add('main-navigation--closed');
+    navigation.classList.remove('main-navigation--opened');
   }
 });
