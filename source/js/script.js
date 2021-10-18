ebdfe705f9e6faf121394fcdf13397a0ff3f9611
juanacardo <juanacardo@gmail.com>
2021-10-18 16:43:22 +0500

    Финальные доработки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,47 @@
 'use strict';
-var navigation = document.querySelector('.main-navigation');
-var navToggle = navigation.querySelector('.main-navigation__toggle');
+var navigationMenu = document.querySelector('.main-navigation');
+var navToggle = navigationMenu.querySelector('.main-navigation__toggle');
+var callbackForm = document.querySelector('.form');
+var telInput = callbackForm.querySelector('input[type="tel"]');
+var inputErrorColor = '#ff0001';
 
-navigation.classList.remove('main-navigation--nojs');
+// Меню-бургер
+navigationMenu.classList.remove('main-navigation--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navigation.classList.contains('main-navigation--closed')) {
-    navigation.classList.remove('main-navigation--closed');
-    navigation.classList.add('main-navigation--opened');
+  if (navigationMenu.classList.contains('main-navigation--closed')) {
+    navigationMenu.classList.remove('main-navigation--closed');
+    navigationMenu.classList.add('main-navigation--opened');
   } else {
-    navigation.classList.add('main-navigation--closed');
-    navigation.classList.remove('main-navigation--opened');
+    navigationMenu.classList.add('main-navigation--closed');
+    navigationMenu.classList.remove('main-navigation--opened');
   }
 });
+
+// Функция визульного отображения ошибки валидации
+var setError = function (input) {
+  input.style.borderColor = inputErrorColor;
+};
+
+var removeError = function (input) {
+  input.setCustomValidity('');
+  input.style.borderColor = '';
+};
+
+// Валидация формы
+var onTelInput = function () {
+  var inputValue = telInput.value;
+  var re = /[0-9]/;
+  if (re.test(inputValue) === false) {
+    telInput.setCustomValidity('Номер телефона должен содержать цифры');
+    setError(telInput);
+  } else {
+    removeError(telInput);
+  }
+  if (telInput.value === '') {
+    removeError(telInput);
+  }
+  telInput.reportValidity();
+};
+
+telInput.addEventListener('input', onTelInput);
